ae34633c5a24aa2a7ced546224a033ca2008f0d5
sai <rge2aiit@gmail.com>
2018-01-15 07:01:54 +0530

    making it same as alienhu initcouch

--- initCouchDB.js
+++ initCouchDB.js
@@ -1,12 +1,12 @@
 'use strict';
 
 require('shelljs/global');
-var fetch = require('node-fetch');
-var urlHelper = require('url');
+const fetch = require('node-fetch');
+const urlHelper = require('url');
 
-var addCorsToCouch = require('add-cors-to-couchdb');
+const addCorsToCouch = require('add-cors-to-couchdb');
 
-var yargs = require('yargs')
+let yargs = require('yargs')
   .alias('p', 'password')
   .describe('p', 'admin password')
   .alias('u', 'username')
@@ -17,16 +17,15 @@ var yargs = require('yargs')
   .usage('\nUsage:\n  $0 [url] [options]')
   .example('$0', 'update couch at http://127.0.0.1:5984')
   .example('$0 http://example.com -u username -p password', 'update with auth');
-var argv = yargs.argv;
+let argv = yargs.argv;
 if (argv.h) {
   yargs.showHelp();
   return process.exit(0);
 }
 
-var url2 = 'http://127.0.0.1:5984';
-var url = url2 + '/_node/couchdb@localhost/';
-
-var auth = {
+let url2 = 'http://127.0.0.1:5984';
+let url = url2 + '/_node/couchdb@localhost/';
+let auth = {
   u: 'couchadmin',
   p: 'test'
 };
@@ -38,32 +37,40 @@ if (argv.p && argv.u) {
   };
 }
 
-var formattedUrl = formatUrl(url, auth);
-var formattedUrl2 = formatUrl(url2, auth);
-var securityDoc = '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}';
-var authHandlerDoc = '"{couch_httpd_oauth, oauth_authentication_handler}, {couch_httpd_auth, proxy_authentification_handler}, {couch_httpd_auth, cookie_authentication_handler}, {couch_httpd_auth, default_authentication_handler}"';
-var useUsersDoc = '"true"';
+let formattedUrl = formatUrl(url, auth);
+let formattedUrl2 = formatUrl(url2, auth);
 
-//todo : handle creating user if user is already created
-return createAdminUser(url).then(function (resp) {
-  //return Promise.resolve().then(function () {
-  return step2();
-}).catch(function (err) {
-  return step2();
-});
+let securityDoc = '{ "admins": { "names": [], "roles": ["admin"]}, "members": { "names": [], "roles": []}}';
+let authHandlerDoc = '"{couch_httpd_oauth, oauth_authentication_handler}, {couch_httpd_auth, proxy_authentification_handler}, {couch_httpd_auth, cookie_authentication_handler}, {couch_httpd_auth, default_authentication_handler}"';
+let useUsersDoc = '"true"';
 
-function step2() {
-  var promisesArray = [];
-  var corsUrl = formattedUrl2.substr(0, formattedUrl.length - 1);
-  promisesArray.push(addCorsToCouch(corsUrl));
-  promisesArray.push(addUserRoles(formattedUrl2));
-  promisesArray.push(updateConfigParams(formattedUrl));
+return configure();
 
-  return Promise.all(promisesArray);
+async function configure() {
+  let bCouch2 = await isCouch2();
+  if (!bCouch2) {
+    formattedUrl = formattedUrl2;
+    url = url2 + '/';
+  }
+  await createAdminUser(url); //exception is not thrown
+  let corsUrl = formattedUrl2.substr(0, formattedUrl2.length - 1);
+  await addCorsToCouch(corsUrl);
+  await updateConfigParams(formattedUrl, bCouch2);
+  await addUserRoles(formattedUrl2, bCouch2);
+}
+
+function isCouch2() {
+  return fetch('http://localhost:5984/_membership').then(function (resp) {
+    if (resp.status !== 200) {
+      return false;
+    } else {
+      return true;
+    }
+  });
 }
 
 function formatUrl(baseUrl, auth) {
-  var urlObject = urlHelper.parse(baseUrl);
+  let urlObject = urlHelper.parse(baseUrl);
 
   if (auth) {
     urlObject.auth = auth.u + ':' + auth.p;
@@ -72,53 +79,69 @@ function formatUrl(baseUrl, auth) {
   return urlHelper.format(urlObject);
 }
 
-function updateConfig(path, reqData) {
-  return fetch(path, {
-    method: 'PUT',
-    body: reqData
-  }).then(function (resp) {
-    if (resp.status === 200) {
+function updateConfig(path, reqData, bLog) {
+  let param = {
+    method: 'PUT'
+  };
+  if (reqData) {
+    param.body = reqData;
+  }
+
+  return fetch(path, param).then(function (resp) {
+    if (resp.status === 200 || resp.status === 201) {
       return;
     }
     return resp.text().then(function (text) {
       throw new Error('status ' + resp.status + ' ' + text);
     });
   }).catch(function (err) {
-    throw err;
+    if (bLog) {
+      console.log(err);
+    }
+    console.log('<' + path + '> Update Failed. Try Again. No Exception is thrown. But Fatal.');
   });
 }
 
 function createAdminUser(_url) {
-  var path = _url + '_config/admins/' + auth.u;
-  var password = JSON.stringify(auth.p);
+  let path = _url + '_config/admins/' + auth.u;
+  let password = JSON.stringify(auth.p);
 
-  return updateConfig(path, password);
-}
-
-function addUserRoles(url) {
-  var userPromisesArray = [];
-  userPromisesArray.push(updateConfig(url + '_users'));
-  userPromisesArray.push(updateConfig(url + '_replicator'));
-  return Promise.all(userPromisesArray).then(function () {
-    var path = url + '_users/_security';
-    return updateConfig(path, securityDoc);
-  }).catch(function () {
-    var path = url + '_users/_security';
-    return updateConfig(path, securityDoc);
+  return updateConfig(path, password).catch(function () {
+    console.log('admin password has already been setup');
   });
 }
 
-function updateConfigParams(url) {
+async function addUserRoles(_url, bCouch2) {
+  if (bCouch2) {
+    await updateConfig(_url + '_users', undefined, true);
+    await updateConfig(_url + '_replicator');
+  }
 
-  var path1 = url + '/_config/http/authentication_handlers/';
-  var path2 = url + '/_config/couch_httpd_oauth/use_users_db';
-  var path3 = url + '/_config/httpd/bind_address';
-  var path4 = url + '/_config/chttpd/bind_address';
+  let path = _url + '_users/_security';
+  await updateConfig(path, securityDoc, true);
+}
 
-  var promisesArray = [];
-  promisesArray.push(updateConfig(path1, authHandlerDoc));
-  promisesArray.push(updateConfig(path2, useUsersDoc));
-  promisesArray.push(updateConfig(path3, '"0.0.0.0"'));
-  promisesArray.push(updateConfig(path4, '"0.0.0.0"'));
-  return Promise.all(promisesArray);
+function updateConfigParams(_url, bCouch2) {
+
+  let path1 = _url + '_config/http/authentication_handlers/';
+  let path2 = _url + '_config/couch_httpd_oauth/use_users_db';
+  let path3 = _url + '_config/httpd/bind_address';
+  let path4 = _url + '_config/chttpd/bind_address';
+  let path5 = _url + '_config/couchdb/delayed_commits';
+  // let path6 = _url + '_config/couchdb/database_dir';
+  let path7 = _url + '_config/logal/level';
+
+  //updateConfig exception is not throw .. just console message
+  await updateConfig(path1, authHandlerDoc);
+  await updateConfig(path2, useUsersDoc);
+  await updateConfig(path3, '"0.0.0.0"');
+  if (bCouch2) {
+    await updateConfig(path4, '"0.0.0.0"');
+    await updateConfig(path7, '"none"');
+  }
+  await updateConfig(path5, '"false"');
+  // let dbPath = '"../../data"';
+  // if (bCouch2) {
+  //   dbPath = '"../data"';
+  // }
 }
